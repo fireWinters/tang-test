b488c621fcd3c603f09ec70f05275c93e89fb43a
唐王瑶 <tangwangyao@hualala.com>
2022-06-13 20:27:02 +0800

    feat: add phantomjs and karma-phantomjs-launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: 唐王瑶
+ * @Email：tangwangyao@hualala.com
+ * @Date: 2022-06-13 12:53:49
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /tang-test/karma.conf.js
+ */
 // Karma configuration
 // Generated on Mon Jun 13 2022 12:53:49 GMT+0800 (中国标准时间)
 
@@ -59,7 +66,8 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    // 安装无头浏览器后，在无头浏览器中国呢跑测试，这个参数就要配置成true
+    singleRun: true,
 
     // Concurrency level
     // how many browser instances should be started simultaneously
