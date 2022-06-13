4240bf8eec22152c85b698608c880e08f8cd3a0f
唐王瑶 <tangwangyao@hualala.com>
2022-06-13 20:26:03 +0800

    feat: add test demo file

---
+++ tests/unit/sum.test.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: 唐王瑶
+ * @Email：tangwangyao@hualala.com
+ * @Date: 2022-06-13 12:41:38
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /tang-test/tests/unit/sum.test.js
+ */
+describe("Sum", function () {
+    if ('测试一下sum函数',function(){
+        expect(sum(1,2).toBe(4))
+    });
+})
